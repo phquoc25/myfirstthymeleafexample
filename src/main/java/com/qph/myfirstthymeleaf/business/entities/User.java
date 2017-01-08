f25151ec637b6e05ea6c5ebfe4d9d4a5ca8e2a21
quoc phan <phquoc25@gmail.com>
2017-01-08 22:22:49 +0700

    Commit the entities and repositories

---
+++ src/main/java/com/qph/myfirstthymeleaf/business/entities/User.java
@@ -0,0 +1,45 @@
+package com.qph.myfirstthymeleaf.business.entities;
+
+/**
+ * Created by quoc on 08/01/2017.
+ */
+public class User {
+
+    private String firstName = null;
+    private String lastName = null;
+    private String nationality = null;
+    private Integer age = null;
+
+
+    public User(final String firstName, final String lastName,
+                final String nationality, final Integer age) {
+        super();
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationality = nationality;
+        this.age = age;
+    }
+
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    public String getNationality() {
+        return this.nationality;
+    }
+
+    public Integer getAge() {
+        return this.age;
+    }
+
+}
+
