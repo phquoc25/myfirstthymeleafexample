f0bf6d5cbdeadcffe6abe528515e031449e2fbd8
quoc phan <phquoc25@gmail.com>
2017-01-10 00:52:47 +0700

    Implement the Template engine and template resolver

---
+++ src/main/java/com/qph/myfirstthymeleaf/web/filter/GTVGFilter.java
@@ -0,0 +1,92 @@
+package com.qph.myfirstthymeleaf.web.filter;
+
+import com.qph.myfirstthymeleaf.business.entities.User;
+import com.qph.myfirstthymeleaf.web.application.GTVGApplication;
+import com.qph.myfirstthymeleaf.web.controller.IGTVGController;
+import org.thymeleaf.ITemplateEngine;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by quoc on 09/01/2017.
+ */
+public class GTVGFilter implements Filter {
+
+    private ServletContext servletContext;
+    private GTVGApplication application;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        servletContext = filterConfig.getServletContext();
+        application = new GTVGApplication(this.servletContext);
+    }
+
+    private static void addUserToSession(final HttpServletRequest request) {
+        // Simulate a real user session by adding a user object
+        request.getSession(true).setAttribute("user", new User("John", "Apricot", "Antarctica", null));
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        if (!process((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse)) {
+            filterChain.doFilter(servletRequest, servletResponse);
+        }
+    }
+
+    private boolean process(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+        try {
+            // This prevents triggering engine executions for resource URLs
+            if (request.getRequestURI().startsWith("/css") ||
+                    request.getRequestURI().startsWith("/images") ||
+                    request.getRequestURI().startsWith("/favicon")) {
+                return false;
+            }
+        /*
+             * Query controller/URL mapping and obtain the controller
+             * that will process the request. If no controller is available,
+             * return false and let other filters/servlets process the request.
+             */
+            IGTVGController controller = this.application.resolveControllerForRequest(request);
+            if (controller == null) {
+                return false;
+            }
+
+         /*
+             * Obtain the TemplateEngine instance.
+             */
+            ITemplateEngine templateEngine = this.application.getTemplateEngine();
+
+        /*
+             * Write the response headers
+             */
+            response.setContentType("text/html;charset=UTF-8");
+            response.setHeader("Pragma", "no-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expires", 0);
+
+            /*
+             * Execute the controller and process view template,
+             * writing the results to the response writer.
+             */
+            controller.process(
+                    request, response, this.servletContext, templateEngine);
+
+            return true;
+        } catch (Exception e) {
+            try {
+                response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            } catch (final IOException ignored) {
+                // Just ignore this
+            }
+            throw new ServletException(e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
