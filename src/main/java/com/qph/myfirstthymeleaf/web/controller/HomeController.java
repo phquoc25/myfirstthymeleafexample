118abdd42fc42871eeec126d657e2b36fff5edf5
quoc phan <phquoc25@gmail.com>
2017-03-01 22:53:29 +0700

    Implement the home controller and main filter

--- src/main/java/com/qph/myfirstthymeleaf/web/controller/HomeController.java
+++ src/main/java/com/qph/myfirstthymeleaf/web/controller/HomeController.java
@@ -1,17 +1,23 @@
 package com.qph.myfirstthymeleaf.web.controller;
 
 import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Calendar;
 
 /**
  * Created by quoc on 10/01/2017.
  */
 public class HomeController implements IGTVGController {
     @Override
-    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) {
+    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) throws IOException {
+        WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+        ctx.setVariable("today", Calendar.getInstance());
 
+        templateEngine.process("home", ctx, response.getWriter());
     }
 }
--- src/main/java/com/qph/myfirstthymeleaf/web/controller/IGTVGController.java
+++ src/main/java/com/qph/myfirstthymeleaf/web/controller/IGTVGController.java
@@ -5,10 +5,11 @@ import org.thymeleaf.ITemplateEngine;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by quoc on 09/01/2017.
  */
 public interface IGTVGController {
-    void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine);
+    void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) throws IOException;
 }
