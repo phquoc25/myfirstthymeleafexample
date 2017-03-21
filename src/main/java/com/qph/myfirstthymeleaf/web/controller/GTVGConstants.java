f2bc494f9aeced14da253b94fee4961b1b2188de
quoc phan <phquoc25@gmail.com>
2017-03-22 01:07:55 +0700

    [feature-sp-st2] Showing the list of products

---
+++ src/main/java/com/qph/myfirstthymeleaf/web/controller/GTVGConstants.java
@@ -0,0 +1,9 @@
+package com.qph.myfirstthymeleaf.web.controller;
+
+/**
+ * Created by quoc on 16/03/2017.
+ */
+public class GTVGConstants {
+    public static final String PAGE_LIST = "product/list";
+    public static final String PAGE_HOME = "home";
+}
--- src/main/java/com/qph/myfirstthymeleaf/web/controller/ProductListController.java
+++ src/main/java/com/qph/myfirstthymeleaf/web/controller/ProductListController.java
@@ -1,17 +1,32 @@
 package com.qph.myfirstthymeleaf.web.controller;
 
+import com.qph.myfirstthymeleaf.business.entities.Product;
+import com.qph.myfirstthymeleaf.business.services.ProductService;
 import org.thymeleaf.ITemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by quoc on 10/01/2017.
  */
 public class ProductListController implements IGTVGController {
-    @Override
-    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) {
 
+    private ProductService productService;
+
+    public ProductListController(){
+        productService = new ProductService();
+    }
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) throws IOException {
+        WebContext webContext = new WebContext(request, response, servletContext, request.getLocale());
+        List<Product> productList = productService.findAll();
+        webContext.setVariable("prods", productList);
+        templateEngine.process(GTVGConstants.PAGE_LIST, webContext, response.getWriter());
     }
 }
