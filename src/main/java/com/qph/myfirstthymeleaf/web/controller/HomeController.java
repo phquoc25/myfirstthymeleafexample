588cc8ec1ebc2c3848b28c077f10c8eaf6cd3e96
quoc phan <phquoc25@gmail.com>
2017-03-22 01:07:55 +0700

    [feature-sp-st2] Showing the list of products

--- src/main/java/com/qph/myfirstthymeleaf/web/controller/HomeController.java
+++ src/main/java/com/qph/myfirstthymeleaf/web/controller/HomeController.java
@@ -13,11 +13,12 @@ import java.util.Calendar;
  * Created by quoc on 10/01/2017.
  */
 public class HomeController implements IGTVGController {
+
     @Override
     public void process(HttpServletRequest request, HttpServletResponse response, ServletContext servletContext, ITemplateEngine templateEngine) throws IOException {
         WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
         ctx.setVariable("today", Calendar.getInstance());
 
-        templateEngine.process("home", ctx, response.getWriter());
+        templateEngine.process(GTVGConstants.PAGE_HOME, ctx, response.getWriter());
     }
 }
